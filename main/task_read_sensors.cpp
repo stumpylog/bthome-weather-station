0a957b9179194fa96d31cbd854acdb2a822085e4
Trenton Holmes <holmes.trenton@gmail.com>
2022-09-20 20:01:42 -0700

    Changes chip back to an esp32

--- main/task_read_sensors.cpp
+++ main/task_read_sensors.cpp
@@ -30,15 +30,22 @@ int i2c_master_port = 0;
 
 void i2c_init(void)
 {
+    uint8_t buffer[18];
+
     ESP_ERROR_CHECK(i2c_param_config(i2c_master_port, &conf));
     i2c_driver_install(i2c_master_port, I2C_MODE_MASTER, 0, 0, ESP_INTR_FLAG_IRAM);
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (SHT4x_DEFAULT_ADDR << 1) | I2C_MASTER_READ, true);
-    // i2c_master_read(cmd, buf.data, buf.len, i == cnt - 1 ? I2C_MASTER_LAST_NACK : I2C_MASTER_ACK);
+    i2c_master_read(cmd, &buffer[0], 18, I2C_MASTER_ACK);
     i2c_master_stop(cmd);
+
     i2c_master_cmd_begin(i2c_master_port, cmd, 20 / portTICK_PERIOD_MS);
+
     i2c_cmd_link_delete(cmd);
+
+
 
     // float t_ticks = (uint16_t)readbuffer[0] * 256 + (uint16_t)readbuffer[1];
     // float rh_ticks = (uint16_t)readbuffer[3] * 256 + (uint16_t)readbuffer[4];
